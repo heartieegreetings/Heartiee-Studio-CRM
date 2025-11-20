a6339c2eb06b03b5a5a9e48aa6422cb042066dfe
heartieegreetings <clubheartiee@gmail.com>
2025-11-20 10:48:42 +0000

    Temp Firebase isolation

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,7 @@
 // firebase.ts
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
 
 // PASTE YOUR COPIED CONFIG HERE
 // Import the functions you need from the SDKs you need
